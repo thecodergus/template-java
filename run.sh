c00142e22906d173b173c9296f7b033298d4f40f
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2025-07-30 13:29:00 -0300

    Adicionando arquivos

--- run.sh
+++ run.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-echo "🔍 Garantindo que o Gradle Wrapper está executável..."
-chmod +x ./gradlew
 
 echo "🔧 Baixando toolchain e dependências..."
-./gradlew --no-daemon clean build
+gradlew --no-daemon clean build
 
 echo "✅ Build e testes concluídos!"
 
 echo "🚀 Executando aplicação:"
-./gradlew --no-daemon run
+gradlew --no-daemon run
